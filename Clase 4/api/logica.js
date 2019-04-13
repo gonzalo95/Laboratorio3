bbb5765cc141aaeb5b0d76914ac8cecff70027bd
gonzalo <gonzalojgreco@gmail.com>
2019-04-12 22:13:05 -0300

    clase 4 y 5

---
+++ Clase 4/api/logica.js
@@ -0,0 +1,44 @@
+var xml = new XMLHttpRequest(); // Variable global
+
+window.addEventListener("load", function()
+{
+	document.getElementById("btn").addEventListener("click", enviarGet);
+})
+
+function callback()
+{
+	if (xml.readyState == 4) 
+	{
+		if (xml.status == 200) 
+		{
+			var respuesta = xml.responseText;
+			if (respuesta == "true") 
+			{
+				alert("Login ok");
+			}
+			else if (respuesta == "false") 
+			{
+				alert("Usuario o contraseña incorrectos");	
+			}
+			else
+			{
+				alert("Error del servidor: " + respuesta);
+			}
+		}
+	}
+}
+
+function enviarGet()
+{
+	var user = document.getElementById("usr").value;
+	var password = document.getElementById("pass").value;
+
+	if (user != "" && password != "") 
+	{
+		var parametros = "?usr=" + user +"&pass=" + password; // "?usr" En GET porque viaja por url
+		xml.open("POST", "http://localhost:3000/loginUsuario" + parametros, true); // xml.open("GET", "http://localhost:3000/loginUsuario" + parametros, true);
+		xml.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); // Obviado en GET
+		xml.onreadystatechange = callback;
+		xml.send(parametros); //xml.send();
+	}
+}
